0355156753d5eb5d11432d0043e9e9e56ab5811d
keiz1213 <fukuoka00tsc@gmail.com>
2023-01-31 19:43:46 +0900

    axios.js追加

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,15 @@
 FROM node:16.13.1-alpine
 
 ARG WORKDIR
+ARG API_URL
 # ARG CONTAINER_PORT
 
 ENV HOME=/${WORKDIR} \
     LANG=C.UTF-8 \
     TZ=Asia/Tokyo \
     # 外部(ブラウザ)から(コンテナ内のnuxtに)参照できるように
-    HOST=0.0.0.0
+    HOST=0.0.0.0 \
+    API_URL=${API_URL}
 
 # ENV check（このRUN命令は確認のためなので無くても良い）
 # RUN echo ${HOME}
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,43 @@
-<template>
+<!-- <template>
   <Tutorial />
 </template>
 
 <script>
 export default {
   name: 'IndexPage'
+}
+</script> -->
+
+<template>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg () {
+      this.$axios.$get('/api/v1/hello')
+        .then(res => this.msgs.push(res))
+    }
+  }
 }
 </script>
