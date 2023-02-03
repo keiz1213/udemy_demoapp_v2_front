226d3dff9146ea4d2dec2da6d9a64637c25d5d8a
keiz1213 <fukuoka00tsc@gmail.com>
2023-02-03 14:08:27 +0900

    ツールバー作成

--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -1,8 +1,43 @@
 <template>
-  <div>
-    HomeAppBar.vue
-  </div>
+  <v-app-bar
+    app
+    dark
+  >
+    <app-logo />
+    <v-toolbar-title>
+      {{ appName }}
+    </v-toolbar-title>
+    <v-spacer />
+
+    <v-toolbar-items class="ml-2">
+      <v-btn
+        v-for="(menu, i) in menus"
+        :key="`menu-btn-${i}`"
+        text
+      >
+        {{ $t(`menus.${menu.title}`) }}
+      </v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
 </template>
 
 <script>
+export default {
+  props: {
+    menus: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  },
+  // dataの引数にはcontextなどのvue.jsが用意した引数を取ることができる
+  // data (context: { $config: { appName: "Bizplanner" } })
+  data ({ $config: { appName } }) {
+    return {
+      // 本来は appName: appName javascriptではキーとバリューが同じ場合省略気泡が使える
+      appName
+    }
+  }
+}
 </script>
