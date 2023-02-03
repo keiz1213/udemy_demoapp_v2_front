a3f350b4f868d3027223623ecfff556a335bfc4f
keiz1213 <fukuoka00tsc@gmail.com>
2023-02-03 15:37:03 +0900

    HomeAppBarにイベント追加

--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -1,9 +1,14 @@
 <template>
   <v-app-bar
     app
-    dark
+    :dark="!isScrollPoint"
+    :height="homeAppBarHeight"
+    :color="toolbarStyle.color"
+    :elevation="toolbarStyle.elevation"
   >
-    <app-logo />
+    <app-logo
+      @click.native="$vuetify.goTo('#scroll-top')"
+    />
     <v-toolbar-title>
       {{ appName }}
     </v-toolbar-title>
@@ -14,6 +19,7 @@
         v-for="(menu, i) in menus"
         :key="`menu-btn-${i}`"
         text
+        @click="$vuetify.goTo(`#${menu.title}`)"
       >
         {{ $t(`menus.${menu.title}`) }}
       </v-btn>
@@ -29,14 +35,45 @@ export default {
       default: () => {
         return []
       }
+    },
+    imgHeight: {
+      type: Number,
+      default: 0
     }
   },
   // dataの引数にはcontextなどのvue.jsが用意した引数を取ることができる
   // data (context: { $config: { appName: "Bizplanner" } })
-  data ({ $config: { appName } }) {
+  data ({ $config: { appName }, $store }) {
     return {
       // 本来は appName: appName javascriptではキーとバリューが同じ場合省略気泡が使える
-      appName
+      appName,
+      scrollY: 0,
+      homeAppBarHeight: $store.state.styles.homeAppBarHeight
+    }
+  },
+  computed: {
+    // 500 - 56 = 444px超の場合、trueを返す
+    isScrollPoint () {
+      return this.scrollY > (this.imgHeight - this.homeAppBarHeight)
+    },
+    toolbarStyle () {
+      const color = this.isScrollPoint ? 'white' : 'transparent'
+      const elevation = this.isScrollPoint ? 4 : 0
+      return {
+        color,
+        elevation
+      }
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll () {
+      this.scrollY = window.scrollY
     }
   }
 }
---
+++ store/index.js
@@ -0,0 +1,16 @@
+// vueアプリ全体で共通する変数やメソッドなどを置くところ
+
+// 変数
+export const state = () => ({
+  styles: {
+    homeAppBarHeight: 56
+  }
+})
+// 算出プロパティ
+export const getters = {}
+
+// stateの値を更新する場所
+export const mutations = {}
+
+// メソッド
+export const actions = {}
